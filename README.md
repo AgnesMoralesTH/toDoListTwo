092e5d88db348e3d4502404d8517bcc49adb209e
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-05-08 21:30:04 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ To Do List Two
---
+++ todo_list/base/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib.auth import login
+# Create your views here.
